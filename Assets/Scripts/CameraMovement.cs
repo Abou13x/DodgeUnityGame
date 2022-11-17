b09ca146735987f4bbbb166024929b26207dd8e3
Abou13x <aboudiomande13@outlook.com>
2022-11-17 01:00:33 -0500

    new commit

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -4,9 +4,22 @@ using UnityEngine;
 public class CameraMovement : MonoBehaviour
 {
     public Transform player;
+    public Vector3 cameraOffset;
+    public float smoothFactor = 0.5f;
+
+
     // Update is called once per frame
-    void Update()
+
+    void Start()
     {
+        cameraOffset = transform.position - player.transform.position;
+    }
+    void LateUpdate()
+    {
+        Vector3 newPosition = player.transform.position + cameraOffset;
+        transform.position = Vector3.Slerp(transform.position, newPosition, smoothFactor);
+     
+        transform.LookAt(player);
         
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿
 using System.Collections;
 using System.Collections.Generic;
 using System.Security.Cryptography;
@@ -7,20 +7,27 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField]
-    private float speed = 2.0f;
+    //private Rigidbody rb;
+    //public float constantSpeed;
+    //[SerializeField]
+    //private float speed = .05f;
     [SerializeField]
     private float jumpHeight = 10;
     [SerializeField]
     private float gravity = 9.81f;
-
+    [SerializeField]
     private Vector2 moveVector;
+
     private CharacterController characterController;
     private Animator animator;
     private float verticalVelocity;
+    [SerializeField]
+    public float speed = 5.0F;
+
     // Start is called before the first frame update
     void Start()
     {
+
         characterController = GetComponent<CharacterController>();
         animator = GetComponent<Animator>();
     }
@@ -28,9 +35,9 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+        characterController.SimpleMove(forward * speed);
         Move();
-       // rb.AddForce(0, 0, 200 * Time.deltaTime);
-
     }
 
     public void OnMove(InputAction.CallbackContext context)
@@ -50,26 +57,27 @@ public class PlayerController : MonoBehaviour
         Vector3 move = transform.right*moveVector.x + transform.forward*moveVector.y + transform.up*verticalVelocity;
         characterController.Move(move*speed*Time.deltaTime);
     }
+
     public void OnJump(InputAction.CallbackContext context)
     {
         if(characterController.isGrounded && context.performed)
         {
             jump();
+            animator.Play("Jump");
+            
         }
-        if (moveVector.magnitude > 0)
-        {
-            animator.SetBool("IsJumping", true);
-        }
-        else
-        {
-            animator.SetBool("IsJumping", false);
-        }
+        //if (moveVector.magnitude > 0)
+        //{
+        //    animator.SetBool("IsJumping", true);
+        //}
+        //else
+        //{
+        //    animator.SetBool("IsJumping", false);
+        //}
     }
 
     private void jump()
     {
         verticalVelocity = Mathf.Sqrt(jumpHeight * gravity);
     }
-
-    
 }
